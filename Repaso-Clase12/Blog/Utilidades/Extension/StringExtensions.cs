9d126630aa47e2d4d54267aac8edb70319766feb
Gabicapo01 <glc2401@gmail.com>
2021-03-16 23:37:22 +0000

    clase 12

---
+++ Repaso-Clase12/Blog/Utilidades/Extension/StringExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilidades.Extension
+{
+    public static class StringExtensions
+    {
+        public static int ToInt32(this string valor)
+        {
+            int resultado = 0;
+            try
+            {
+                resultado = Convert.ToInt32(valor);
+            }
+            catch
+            {
+
+                resultado = 0;
+            }
+            return resultado;
+        }
+        public static string Concatenar(this string valor, string nuevoValor)
+        {
+            return valor + nuevoValor;
+        }
+    }
+}
